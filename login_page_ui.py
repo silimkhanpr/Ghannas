ec8ded5c69497fba5804bf8fa0483f44732704f3
Nandan25 <nandanprabhudesai@gmail.com>
2019-06-06 12:02:24 +0530

    Add files via upload

--- login_page_ui.py
+++ login_page_ui.py
@@ -5,7 +5,7 @@ from DatabaseQuery import db_point
 from test import encrypt_code
 import os
 
-master =Tk()
+master = Tk()
 master.geometry("350x350")
 master.title("Login")
 
@@ -20,6 +20,7 @@ label3.place(relx=0.12, rely=0.0, height=35, width=150)
 
 entry1 = ttk.Entry(master)
 entry1.place(relx=0.45, rely=0.15, height=25, width=125)
+entry1.focus()
 
 entry2 = ttk.Entry(master,show="*")
 entry2.place(relx=0.45, rely=0.25, height=25, width=125)
@@ -28,8 +29,8 @@ label4 = Label(master, fg="green")
 label4.place(relx=0.35, rely=0.45, height=30, width=120)
 
 def clicked(event=None):
-    username = "pranavsilimkhan"#entry1.get()
-    password = "password"#entry2.get()
+    username = entry1.get()
+    password = entry2.get()
     pass1 = encrypt_code(password)
     uname = str(username)
     sql = "select * from users where username='"+uname+"'"
@@ -43,13 +44,15 @@ def clicked(event=None):
                  if utype == "standard":
                     print(uname+" Logged in as standard user")
                     label4.configure(text="Login Success!")
-                    user="python user_chat_page.py"
-                    # print (user+" "+str(eid))
+                    user = "python user_chat_page.py"
+                    master.destroy()
                     os.system(user+" "+str(eid))
+
                  elif utype == "admin":
 
                     print(uname+" Logged in as admin")
                     user = "python admin_chat_page.py"
+                    master.destroy()
                     os.system(user+" "+str(eid))
                     label4.configure(text="Login Success!")
 
