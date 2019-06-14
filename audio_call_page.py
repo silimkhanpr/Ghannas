21908a4bbdd140a5117fc2a1e72be86d1dd39dbb
anaynaik <48745704+anaynaik@users.noreply.github.com>
2019-06-14 18:59:47 +0530

    Add files via upload

--- audio_call_page.py
+++ audio_call_page.py
@@ -1,13 +1,14 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import sys
 import os
-from DatabaseQuery import db_point
-from socket import socket, AF_INET, SOCK_STREAM
-from threading import Thread
-import pyaudio
+#from DatabaseQuery import db_point
+#from socket import socket, AF_INET, SOCK_STREAM
+#from threading import Thread
+#import pyaudio
 from array import array
-from test import server_config
+#from test import server_config
 
 master = Tk()
 master.geometry("300x300")
@@ -21,10 +22,31 @@ if records:
     for x in records:
         name = x[0]
        
-Label1 = Label(master, text=name)
+Label1 = Label(master, text="Pranav Silimkhan")
 Label1.place(relx=0.25, rely=0.45, height=100, width=180)
 Label1.config(font=("Times New Roman", 15))
- 
+
+def update_timeText():
+    if (state):
+        global timer
+        timer[2] += 1
+        if (timer[2] >= 100):
+            timer[2] = 0
+            timer[1] += 1
+        if (timer[1] >= 60):
+            timer[0] += 1
+            timer[1] = 0
+        timeString = pattern.format(timer[0], timer[1], timer[2])
+        timeText.configure(text=timeString)
+    master.after(10, update_timeText)
+
+state = True
+timer = [0, 0, 0]
+pattern = '{0:02d}:{1:02d}'
+timeText = tk.Label(master, text="00:00", font=("Helvetica", 15))
+timeText.place(relx=0.40, rely=0.68)
+update_timeText()
+
 canvas = Canvas(master, width = 150, height = 150)      
 canvas.pack()      
 img = PhotoImage(file="icon.ppm")
@@ -34,12 +56,9 @@ def end_call():
     master.destroy()
     socket.close(client)
     
-
 Button1 = Button(master, bg="red")
-Button1.place(relx=0.33, rely=0.7, height=40, width=100)
+Button1.place(relx=0.33, rely=0.8, height=40, width=100)
 Button1.configure(text="End Call", command=end_call)
-
-
 
 HOST = server_config(1)
 PORT = 4000
