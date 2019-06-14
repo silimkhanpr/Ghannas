8f03d1f7ace200b3ba61522f1fccc93b818aa428
Nandan25 <nandanprabhudesai@gmail.com>
2019-06-14 13:29:18 +0530

    Add files via upload

--- user_chat_page.py
+++ user_chat_page.py
@@ -5,11 +5,12 @@ from threading import Thread
 from test import split, server_config
 from DatabaseQuery import db_point, message_populate
 import tkinter
-from tkinter.filedialog import askopenfilename
 import os
 
-#eid = sys.argv[1]
 eid = "3"
+#eid = sys.argv[1]
+receiver_Id = ""
+eid = str(eid)
 window = Tk()
 window.title("User Home")
 window.geometry('800x500')
@@ -36,6 +37,7 @@ def receive():
 
 
 def send(event=None):  # event is passed by binders.
+    print(receiver_Id)
     msg = ChatEntry.get()
     ChatEntry.delete(0, 'end')
     my_msg.set("")  # Clears input field.
@@ -69,7 +71,7 @@ def notices():
     os.system('python notices_ui.py')
 
 
-NoticeButton = Button(window, text="Notices", command=notices, bg="grey")
+NoticeButton = Button(window, text="Notices", command=notices, bg="red")
 NoticeButton.place(relx=0.677, rely=0.0, height=35, width=84)
 
 # left side frame
@@ -119,8 +121,10 @@ Label1.config(font=("Times New Roman", 20))
 
 # code when a contact is clicked
 def chat_click(receiverid, fullname):
+    global receiver_Id
+    receiver_Id=receiverid
     print("sender id is " + eid)
-    print("receiver id is " + str(receiverid))
+    print("receiver is is " + str(receiverid))
     msg_list.delete(0, tkinter.END)
     # ContactButton.config(state="disabled")
 
@@ -141,22 +145,15 @@ def chat_click(receiverid, fullname):
     LabelUser = Label(window, text=fullname, fg="red", bg="yellow")
     LabelUser.place(relx=0.282, rely=0.073, height=60, width=220)
     LabelUser.config(font=("Times New Roman", 20))
-    def audio():
-        os.system("python audio_call_page.py"+" "+str(receiverid))
 
-    def file():
-        filename = askopenfilename()
-        print(filename)
-
-    def video():
-        os.system("python clientMedia.py")
-
-    videocall = Button(window, text="Video Call", command=video)
+    videocall = Button(window, text="Video Call")
     videocall.place(relx=0.797, rely=0.1, height=35, width=96)
-    audiocall = Button(window, text="Audio Call", command=audio)
+    audiocall = Button(window, text="Audio Call")
     audiocall.place(relx=0.687, rely=0.1, height=35, width=96)
-    filetransfer = Button(window, text="File Transfer", command=file)
+    filetransfer = Button(window, text="File Transfer")
     filetransfer.place(relx=0.577, rely=0.1, height=35, width=96)
+
+    # when the back button is pressed
 
 
 # list of contacts dynamically retrieved from database
@@ -183,7 +180,7 @@ def changepw():
 logoutbutton = Button(window, text="Logout", command=logout, bg="red")
 logoutbutton.place(relx=0.907, rely=0.0, height=35, width=86)
 
-changepw = Button(window, text="Change Password", bg="grey", command=changepw)
+changepw = Button(window, text="Change Password", bg="green", command=changepw)
 changepw.place(relx=0.777, rely=0.0, height=35, width=106)
 
 HOST, PORT = server_config()
